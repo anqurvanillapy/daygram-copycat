cb9d4f9e4fc25e9f38e77a654c2f0e295cdfe9f7
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-20 00:55:33 +0800

    updated ListView w/ SimpleAdapter

--- app/src/main/java/com/anqurvanillapy/daygramcopycat/MainActivity.java
+++ app/src/main/java/com/anqurvanillapy/daygramcopycat/MainActivity.java
@@ -5,8 +5,19 @@ import android.app.LoaderManager;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<HashMap<String, String>> entryData = new ArrayList<>();
+        HashMap<String, String> item;
+        item = new HashMap<>();
+        item.put("dayOfWeek", "MON");
+        item.put("date", "1");
+        item.put("abstract", "Blue Mondays");
+        entryData.add(item);
+        item = new HashMap<>();
+        item.put("dayOfWeek", "TUE");
+        item.put("date", "2");
+        item.put("abstract", "Today is good.");
+        entryData.add(item);
+
+        String[] entryFrom = {"dayOfWeek", "date", "abstract"};
+        int[] entryTo = {R.id.itemDayOfWeek, R.id.itemDate, R.id.entryAbstract};
+
+        ListView entryListView = (ListView) findViewById(R.id.entryListView);
+        SimpleAdapter entryAdapter = new SimpleAdapter(this,
+                entryData,
+                R.layout.entry_adapter,
+                entryFrom, entryTo);
+        entryListView.setAdapter(entryAdapter);
     }
 }
