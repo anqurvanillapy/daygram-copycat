6adb28f6efb85206c230cc9595e0b53cb3a6a19a
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-28 02:39:03 +0800

    completed (maybe) EntryEditor stateHandler

--- app/src/main/java/com/anqurvanillapy/daygramcopycat/EntryEditorActivity.java
+++ app/src/main/java/com/anqurvanillapy/daygramcopycat/EntryEditorActivity.java
@@ -2,10 +2,13 @@ package com.anqurvanillapy.daygramcopycat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class EntryEditorActivity extends AppCompatActivity {
@@ -15,19 +18,19 @@ public class EntryEditorActivity extends AppCompatActivity {
         final public static int EDIT = 1;
     }
 
-    private void editorStateHandler(Bundle extras) {
+    private void editorStateHandler(int state) {
         EditText editorText = (EditText) findViewById(R.id.editorText);
         RelativeLayout editorFooterBack = (RelativeLayout) findViewById(R.id.editorFooterBack);
-        RelativeLayout editorFooterDone = (RelativeLayout) findViewById(R.id.editorFooterDone);
+        LinearLayout editorFooterDone = (LinearLayout) findViewById(R.id.editorFooterDone);
 
-        switch (extras.getInt("state")) {
+        switch (state) {
             case EditorState.VIEW:
-                editorText.setEnabled(false);
+                editorText.setInputType(InputType.TYPE_NULL);
                 editorFooterDone.setVisibility(View.GONE);
                 editorFooterBack.setVisibility(View.VISIBLE);
                 break;
             case EditorState.EDIT:
-                editorText.setEnabled(true);
+                editorText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
                 editorFooterDone.setVisibility(View.VISIBLE);
                 editorFooterBack.setVisibility(View.GONE);
                 break;
@@ -41,17 +44,27 @@ public class EntryEditorActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            editorStateHandler(extras);
+            editorStateHandler(extras.getInt("state"));
         } else {
             Log.e("EXTRAS", "Extras from Main to EntryEditor cannot be null");
             finish();
         }
 
-        FrameLayout backButton = (FrameLayout) findViewById(R.id.backButton);
+        FrameLayout backButton = (FrameLayout) findViewById(R.id.buttonBack);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        final EditText editorText = (EditText) findViewById(R.id.editorText);
+        editorText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editorText.getInputType() == InputType.TYPE_NULL) {
+                    editorStateHandler(EditorState.EDIT);
+                }
             }
         });
     }
