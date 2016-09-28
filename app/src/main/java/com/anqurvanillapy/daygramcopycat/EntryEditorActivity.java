34c45601e72e3ff1b384e43da3e8458f4de8ee54
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-28 22:21:11 +0800

    updated simple file handlers

--- app/src/main/java/com/anqurvanillapy/daygramcopycat/EntryEditorActivity.java
+++ app/src/main/java/com/anqurvanillapy/daygramcopycat/EntryEditorActivity.java
@@ -1,15 +1,31 @@
 package com.anqurvanillapy.daygramcopycat;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class EntryEditorActivity extends AppCompatActivity {
 
@@ -18,19 +34,21 @@ public class EntryEditorActivity extends AppCompatActivity {
         final public static int EDIT = 1;
     }
 
-    private void editorStateHandler(int state) {
-        EditText editorText = (EditText) findViewById(R.id.editorText);
-        RelativeLayout editorFooterBack = (RelativeLayout) findViewById(R.id.editorFooterBack);
-        LinearLayout editorFooterDone = (LinearLayout) findViewById(R.id.editorFooterDone);
-
+    private void editorStateHandler(int state,
+                                    EditText editorText,
+                                    RelativeLayout editorFooterBack,
+                                    LinearLayout editorFooterDone) {
         switch (state) {
             case EditorState.VIEW:
                 editorText.setInputType(InputType.TYPE_NULL);
+                editorText.setSingleLine(false);
                 editorFooterDone.setVisibility(View.GONE);
                 editorFooterBack.setVisibility(View.VISIBLE);
                 break;
             case EditorState.EDIT:
-                editorText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+                editorText.setInputType(InputType.TYPE_CLASS_TEXT
+                        | InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE);
+                editorText.setSingleLine(false);
                 editorFooterDone.setVisibility(View.VISIBLE);
                 editorFooterBack.setVisibility(View.GONE);
                 break;
@@ -42,15 +60,68 @@ public class EntryEditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_editor);
 
-        Bundle extras = getIntent().getExtras();
+        /**
+         * =========================================================================================
+         * DateTime & File Handlers
+         * =========================================================================================
+         */
+
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+        Date currentLocalTime = cal.getTime();
+
+        DateFormat title = new SimpleDateFormat("EEEE / MMM dd / yyyy");
+        DateFormat time = new SimpleDateFormat("HH:mm a");
+        DateFormat day = new SimpleDateFormat("yyyMMdd");
+        DateFormat month = new SimpleDateFormat("MM");
+        DateFormat year = new SimpleDateFormat("yyyy");
+
+        final String timeString = time.format(currentLocalTime) + " ";
+        String titleString = title.format(currentLocalTime).toUpperCase();
+        final String entryYear = year.format(currentLocalTime);
+        final String entryMonth = month.format(currentLocalTime);
+        final String entryFilename = day.format(currentLocalTime);
+
+        final Context context = getApplicationContext();
+        final File filesDir = context.getFilesDir();
+        final File entryPath = new File(filesDir, "entries");
+        entryPath.mkdirs();
+
+        /**
+         * =========================================================================================
+         * UI Components (w/ Initialization)
+         * =========================================================================================
+         */
+
+        final EditText editorText = (EditText) findViewById(R.id.editorText);
+        final RelativeLayout editorFooterBack = (RelativeLayout) findViewById(R.id.editorFooterBack);
+        final LinearLayout editorFooterDone = (LinearLayout) findViewById(R.id.editorFooterDone);
+
+        final Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            editorStateHandler(extras.getInt("state"));
+            editorStateHandler(extras.getInt("state"),
+                    editorText,
+                    editorFooterBack,
+                    editorFooterDone);
         } else {
+            // FIXME: Ugly self-implemented error logging
             Log.e("EXTRAS", "Extras from Main to EntryEditor cannot be null");
             finish();
         }
 
+        TextView editorHeaderTitle = (TextView) findViewById(R.id.editorHeaderTitle);
+        editorHeaderTitle.setText(titleString);
+
         FrameLayout backButton = (FrameLayout) findViewById(R.id.buttonBack);
+
+        TextView buttonInsertTime = (TextView) findViewById(R.id.buttonInsertTime);
+        TextView buttonEditDone = (TextView) findViewById(R.id.buttonEditDone);
+
+        /**
+         * =========================================================================================
+         * VIEW State EventListeners
+         * =========================================================================================
+         */
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,12 +129,80 @@ public class EntryEditorActivity extends AppCompatActivity {
             }
         });
 
-        final EditText editorText = (EditText) findViewById(R.id.editorText);
+        /**
+         * =========================================================================================
+         * EDIT State EventListeners
+         * =========================================================================================
+         */
+
         editorText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (editorText.getInputType() == InputType.TYPE_NULL) {
-                    editorStateHandler(EditorState.EDIT);
+                    editorStateHandler(EditorState.EDIT,
+                            editorText,
+                            editorFooterBack,
+                            editorFooterDone);
+                }
+            }
+        });
+
+        buttonInsertTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editorText.append(timeString);
+            }
+        });
+
+        buttonEditDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String entryTextString = editorText.getText().toString();
+                if (entryTextString.isEmpty()) {
+                    finish();
+                } else {
+                    File entryMonthPath = new File(entryPath, entryYear + File.separator + entryMonth);
+                    entryMonthPath.mkdirs();
+                    File entryFile = new File(entryMonthPath, entryFilename);
+                    File entryAbstract = new File(entryMonthPath, "abstract");
+
+                    try {
+                        entryFile.createNewFile();
+                        OutputStream osFile = new FileOutputStream(entryFile);
+                        osFile.write(entryTextString.getBytes());
+                        osFile.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        JSONObject jsonAbstract = new JSONObject();
+
+                        // FIXME: This string shortening algorithm is ugly
+                        if (entryTextString.length() > 50) {
+                            String stringAbstract = entryTextString.substring(0, 50);
+
+                            if (stringAbstract.contains(" ")) {
+                                stringAbstract = stringAbstract.substring(0,
+                                        stringAbstract.lastIndexOf(" "));
+                            }
+
+                            jsonAbstract.put(entryFilename, stringAbstract + " ...");
+                        } else {
+                            jsonAbstract.put(entryFilename, entryTextString);
+                        }
+
+                        OutputStream osAbstract = new FileOutputStream(entryAbstract);
+                        osAbstract.write(jsonAbstract.toString().getBytes());
+                        osAbstract.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    editorStateHandler(EditorState.VIEW,
+                            editorText,
+                            editorFooterBack,
+                            editorFooterDone);
                 }
             }
         });
--- app/src/main/java/com/anqurvanillapy/daygramcopycat/MainActivity.java
+++ app/src/main/java/com/anqurvanillapy/daygramcopycat/MainActivity.java
@@ -1,9 +1,16 @@
+/**
+ * FIXME:
+ * - [ ] All exceptions are generalized
+ */
+
 package com.anqurvanillapy.daygramcopycat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +19,36 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * =============================================================================================
+     * Custom ListView Item Adapter
+     * =============================================================================================
+     */
+
     public class EntryItemType {
         final public static int EMPTY = 0;
-        final public static int NONEMPTY = 1;
+        final public static int EMPTY_SUNDAY = 1;
+        final public static int NONEMPTY = 2;
+        final public static int NONEMPTY_SUNDAY = 3;
     }
 
     public class EntryItem {
@@ -51,10 +81,24 @@ public class MainActivity extends AppCompatActivity {
         public String getName() { return name; }
     }
 
+    public class EntryEmptySundayObject {
+        String name;
+
+        public EntryEmptySundayObject(String name) { this.name = name; }
+        public String getName() { return name; }
+    }
+
     public class EntryNonemptyObject {
         String name;
 
         public EntryNonemptyObject(String name) { this.name = name; }
+        public String getName() { return name; }
+    }
+
+    public class EntryNonemptySundayObject {
+        String name;
+
+        public EntryNonemptySundayObject(String name) { this.name = name; }
         public String getName() { return name; }
     }
 
@@ -89,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getViewTypeCount() {
-            return 2;
+            return 4;
         }
 
         @Override
@@ -112,6 +156,19 @@ public class MainActivity extends AppCompatActivity {
 
                     emptyHolder.getEntryEmpty().setText(entryEmptyObject.getName());
                     return convertView;
+                case EntryItemType.EMPTY_SUNDAY:
+                    EntryEmptySundayObject entryEmptySundayObject = (EntryEmptySundayObject) entryObject;
+                    ViewHolderEntryEmptySunday emptySundayHolder;
+                    if (convertView == null) {
+                        convertView = inflater.inflate(R.layout.entry_adapter_empty_red, null, false);
+                        emptySundayHolder = new ViewHolderEntryEmptySunday(convertView);
+                        convertView.setTag(emptySundayHolder);
+                    } else {
+                        emptySundayHolder = (ViewHolderEntryEmptySunday) convertView.getTag();
+                    }
+
+                    emptySundayHolder.getEntryEmptySunday().setText(entryEmptySundayObject.getName());
+                    return convertView;
                 case EntryItemType.NONEMPTY:
                     EntryNonemptyObject entryNonemptyObject = (EntryNonemptyObject) entryObject;
                     ViewHolderEntryNonempty nonemptyHolder;
@@ -124,6 +181,19 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     nonemptyHolder.getEntryNonempty().setText(entryNonemptyObject.getName());
+                    return convertView;
+                case EntryItemType.NONEMPTY_SUNDAY:
+                    EntryNonemptySundayObject entryNonemptySundayObject = (EntryNonemptySundayObject) entryObject;
+                    ViewHolderEntryNonemptySunday nonemptySundayHolder;
+                    if (convertView == null) {
+                        convertView = inflater.inflate(R.layout.entry_adapter_red, null, false);
+                        nonemptySundayHolder = new ViewHolderEntryNonemptySunday(convertView);
+                        convertView.setTag(nonemptySundayHolder);
+                    } else {
+                        nonemptySundayHolder = (ViewHolderEntryNonemptySunday) convertView.getTag();
+                    }
+
+                    nonemptySundayHolder.getEntryNonemptySunday().setText(entryNonemptySundayObject.getName());
                     return convertView;
             }
 
@@ -147,6 +217,23 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        private class ViewHolderEntryEmptySunday {
+            private View row;
+            private TextView entryEmptySunday;
+
+            public ViewHolderEntryEmptySunday(View row) {
+                this.row = row;
+            }
+
+            public TextView getEntryEmptySunday() {
+                if (this.entryEmptySunday == null) {
+                    this.entryEmptySunday = (TextView) row.findViewById(R.id.entryEmptyLabel);
+                }
+
+                return this.entryEmptySunday;
+            }
+        }
+
         private class ViewHolderEntryNonempty {
             private View row;
             private TextView entryNonempty;
@@ -163,29 +250,166 @@ public class MainActivity extends AppCompatActivity {
                 return this.entryNonempty;
             }
         }
+
+        private class ViewHolderEntryNonemptySunday {
+            private View row;
+            private TextView entryNonemptySunday;
+
+            public ViewHolderEntryNonemptySunday(View row) {
+                this.row = row;
+            }
+
+            public TextView getEntryNonemptySunday() {
+                if (this.entryNonemptySunday == null) {
+                    this.entryNonemptySunday = (TextView) row.findViewById(R.id.entryAbstract);
+                }
+
+                return this.entryNonemptySunday;
+            }
+        }
     }
+
+    /**
+     * =============================================================================================
+     * ListView Item Generator
+     * =============================================================================================
+     */
+
+    public ArrayList<EntryItem> listItemGenerator(String year, String month) {
+        ArrayList<EntryItem> entryItems = new ArrayList<>();
+        Context context = getApplicationContext();
+        File entryPath = new File(context.getFilesDir(), "entries");
+        File entryAbstract = new File(entryPath,
+                year+ File.separator + month + File.separator + "abstract");
+        entryPath.mkdirs();
+
+        Calendar cal = new GregorianCalendar(Integer.parseInt(year), Integer.parseInt(month) - 1, 1);
+        int daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        if (entryAbstract.exists()) {
+            try {
+                JSONObject jsonAbstract = new JSONObject(loadJSONFromFile(entryAbstract));
+                String stringAbstract;
+                String key;
+                for (int i = 1; i <= daysInMonth; i++) {
+                    cal.set(Integer.parseInt(year), Integer.parseInt(month) - 1, i);
+                    key = String.format(year + month + "%02d", i);
+
+                    if (jsonAbstract.has(key)) {
+                        stringAbstract = jsonAbstract.getString(key);
+                        if (cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                            entryItems.add(new EntryItem(EntryItemType.NONEMPTY,
+                                    new EntryNonemptyObject(stringAbstract)));
+                        } else {
+                            entryItems.add(new EntryItem(EntryItemType.NONEMPTY_SUNDAY,
+                                    new EntryNonemptySundayObject(stringAbstract)));
+                        }
+                    } else {
+                        if (cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                            entryItems.add(new EntryItem(EntryItemType.EMPTY,
+                                    new EntryEmptyObject(year + month + i)));
+                        } else {
+                            entryItems.add(new EntryItem(EntryItemType.EMPTY_SUNDAY,
+                                    new EntryEmptySundayObject(year + month + i)));
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            for (int i = 1; i <= daysInMonth; i++) {
+                cal.set(Integer.parseInt(year), Integer.parseInt(month) - 1, i);
+
+                if (cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                    entryItems.add(new EntryItem(EntryItemType.EMPTY,
+                            new EntryEmptyObject(year + month + i)));
+                } else {
+                    entryItems.add(new EntryItem(EntryItemType.EMPTY_SUNDAY,
+                            new EntryEmptySundayObject(year + month + i)));
+                }
+            }
+        }
+
+        return entryItems;
+    }
+
+    /**
+     * =============================================================================================
+     * JSON File Loader
+     * =============================================================================================
+     */
+
+     public String loadJSONFromFile(File file) {
+         int size;
+         byte[] buffer;
+         InputStream is;
+         String jsonString = null;
+
+         try {
+             is = new FileInputStream(file);
+             size = is.available();
+             buffer = new byte[size];
+             is.read(buffer);
+             is.close();
+             jsonString = new String(buffer, "UTF-8");
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+
+         return jsonString;
+     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<EntryItem> entryItems = new ArrayList<>();
-        entryItems.add(new EntryItem(EntryItemType.EMPTY, new EntryEmptyObject("WTF")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.EMPTY, new EntryEmptyObject("WTF")));
-        entryItems.add(new EntryItem(EntryItemType.EMPTY, new EntryEmptyObject("WTF")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
-        entryItems.add(new EntryItem(EntryItemType.NONEMPTY, new EntryNonemptyObject("Fuck")));
+        /**
+         * =========================================================================================
+         * DateTime Handlers
+         * =========================================================================================
+         */
 
-        EntryAdapter entryAdapter = new EntryAdapter(this, entryItems);
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+        Date currentLocalTime = cal.getTime();
+
+        DateFormat monthName = new SimpleDateFormat("MMMM");
+        DateFormat month = new SimpleDateFormat("MM");
+        DateFormat year = new SimpleDateFormat("yyyy");
+
+        String currentYear = year.format(currentLocalTime);
+        String currentMonth = month.format(currentLocalTime);
+        String currentMonthName = monthName.format(currentLocalTime).toUpperCase();
+
+        /**
+         * =========================================================================================
+         * UI Components (w/ Initialization)
+         * =========================================================================================
+         */
+
+        final TextView buttonTodayEntry = (TextView) findViewById(R.id.buttonTodayEntry);
+        TextView labelMonth = (TextView) findViewById(R.id.labelMonth);
+        TextView labelYear = (TextView) findViewById(R.id.labelYear);
+        labelMonth.setText(currentMonthName);
+        labelYear.setText(currentYear);
+
+        /**
+         * =========================================================================================
+         * ListView Handlers
+         * =========================================================================================
+         */
+
+        ArrayList<EntryItem> _entryItems = listItemGenerator(currentYear, currentMonth);
+        EntryAdapter entryAdapter = new EntryAdapter(this, _entryItems);
         ListView entryListView = (ListView) findViewById(R.id.entryListView);
         entryListView.setAdapter(entryAdapter);
+
+        /**
+         * =========================================================================================
+         * EventListeners
+         * =========================================================================================
+         */
 
         entryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -197,7 +421,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final TextView buttonTodayEntry = (TextView) findViewById(R.id.buttonTodayEntry);
         buttonTodayEntry.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intentEntryEditor = new Intent(MainActivity.this, EntryEditorActivity.class);
@@ -206,18 +429,5 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intentEntryEditor);
             }
         });
-
-//        File entry_file = new File(getApplicationContext().getFilesDir(), "Dgcpcat");
-//        String entry_filename = "hello";
-//        String entry_content = "what a day";
-//        FileOutputStream fos;
-//
-//        try {
-//            fos = openFileOutput(entry_filename, Context.MODE_PRIVATE);
-//            fos.write(entry_content.getBytes());
-//            fos.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 }
